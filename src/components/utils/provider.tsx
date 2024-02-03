7ee66a0a011f4059e0ae87156d87ec025dffa99b
Raviprasath <itsraviprasath@gmail.com>
2024-02-03 22:36:47 +0530

    Project setup and basic config add

---
+++ src/components/utils/provider.tsx
@@ -0,0 +1,21 @@
+"use client";
+import { ThemeProvider } from "next-themes";
+import { useState, useEffect, ReactNode } from "react";
+
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+export default function Providers({ children }: ProvidersProps) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <>{children}</>;
+  }
+
+  return <ThemeProvider enableSystem={false} >{children}</ThemeProvider>;
+}
