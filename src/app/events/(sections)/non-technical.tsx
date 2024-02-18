6b06b7c5fef0cce8edcffca8382943f78ee1184c
akash e <akash.al21@bitsathy.ac.in>
2024-02-18 17:59:03 +0530

    Contact images added

--- src/app/events/(sections)/non-technical.tsx
+++ src/app/events/(sections)/non-technical.tsx
@@ -15,18 +15,21 @@ const NonTechnical = () => {
       </Text>
       <div className="lg:grid grid-cols-3 place-items-center">
         <EventCard
+          link="https://forms.gle/uvxBnoVhebxXQ1Jt8"
           eventName="Connection"
           description="Individual / Team will be allowed for this event."
           key={0}
           image={connection.src}
         />
         <EventCard
+          link="https://forms.gle/uvxBnoVhebxXQ1Jt8"
           eventName="Lens into Life's Tapestry"
           description="Individual  will be allowed for this event (Photography)."
           key={1}
           image={lens.src}
         />
         <EventCard
+          link="https://forms.gle/uvxBnoVhebxXQ1Jt8"
           eventName="Breaking Boundaries"
           description="Individual will be allowed for this event (Poster Presentation)."
           key={2}
