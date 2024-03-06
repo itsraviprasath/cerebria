226edbe568629c436c5ce67d015c012af9e5350b
pulsar@5338 <itsraviprasath@gmail.com>
2024-03-06 22:22:21 +0530

    Hod name add

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -5,12 +5,30 @@ import Shamanth from "@/../public/images/contact/Shamanth.png";
 import Dhanusri from "@/../public/images/contact/Dhanusri.jpg";
 import Dhayanithi from "@/../public/images/contact/Dhayanithi.jpg";
 import Samyuktha from "@/../public/images/contact/Samyuktha.jpg";
+import Kodieswari from "@/../public/images/contact/Kodieswari.jpg";
 import { Text } from "@/components/ui/Text";
 
 const ContactUs = () => {
   return (
     <section className="my-10">
       <HeroSection name="Contact Us" />
+      {/*  */}
+      <div className="my-20">
+        <Text
+          variant="h2"
+          className="text-center my-24 border-b-4 w-fit m-auto pb-4 border-primary"
+        >
+          Head of the Department
+        </Text>
+        <div className="lg:grid grid-cols-1 place-items-center">
+          <PersonCard
+            pic={Kodieswari}
+            name="Dr Kodieswari A"
+            phone="9629372333"
+            key={0}
+          />
+        </div>
+      </div>
       {/* Organinzing Secretary */}
       <div className="my-20">
         <Text
